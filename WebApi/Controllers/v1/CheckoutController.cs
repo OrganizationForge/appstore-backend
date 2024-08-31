8bf6cf365c69193fe4a3995611cfb6034d336786
Jonaunk <paez.jonathan@outlook.com>
2024-08-31 12:13:25 -0300

    checkout Controller

--- WebApi/Controllers/v1/CheckoutController.cs
+++ WebApi/Controllers/v1/CheckoutController.cs
@@ -5,10 +5,11 @@ using Microsoft.AspNetCore.Mvc;
 namespace WebApi.Controllers.v1
 {
     [ApiVersion("1.0")]
-    public class PaymentsController : BaseApiController
+    public class CheckoutController : BaseApiController
     {
 
         [HttpPost]
+        [Route("Payments")]
         public async Task<IActionResult> CreatePayment([FromBody] CreatePaymentCommand command)
         {
             return Ok(await Mediator.Send(command));
